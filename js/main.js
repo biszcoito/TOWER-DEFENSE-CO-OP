549f5e05bfc0928980886a8e6a2e4d3bbeafe125
LUIS_BZSC <luisfelipesrc4@gmail.com>
2025-07-07 11:23:29 -0300

    Delete js/main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// main.js - Inicia tudo!
-
-document.addEventListener('DOMContentLoaded', () => {
-    const authManager = new AuthManager();
-
-    // Isso é útil para depuração, para poder chamar authManager.logout() no console, por exemplo.
-    window.authManager = authManager;
-});
